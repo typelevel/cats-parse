6263b3182c607e1605bed45d0f4a124299239092
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-18 13:48:26 +0100

    Update munit, munit-scalacheck to 0.7.18 (#73)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.2.0")
-  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.17")
-  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.17")
+  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.18")
+  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.18")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.14.3")
 }
