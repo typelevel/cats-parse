56a99dfb3c6601bd0c1eef55d9e5381bcab2777c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-26 11:15:02 +0100

    Update cats-core to 2.3.0 (#91)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
-  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.2.0")
+  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.3.0")
   lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.19")
   lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.19")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.14.3")
