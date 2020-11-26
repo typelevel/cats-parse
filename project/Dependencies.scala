b148dbb9722a315355fc5278b3eeea4c97a04cf4
Filipe Regadas <filiperegadas@gmail.com>
2020-11-26 10:51:58 +0000

    Cleanup deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,4 +6,9 @@ object Dependencies {
   lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.19")
   lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.19")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.14.3")
+  lazy val fastParse = "com.lihaoyi" %% "fastparse" % "2.3.0"
+  lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
+  lazy val jawnAst = "org.typelevel" %% "jawn-ast" % "1.0.0"
+  lazy val parboiled = "org.parboiled" %% "parboiled" % "2.2.1"
+  lazy val attoCore = "org.tpolecat" %% "atto-core" % "0.8.0"
 }
