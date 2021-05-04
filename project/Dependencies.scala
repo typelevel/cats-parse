0e1ab388532a2d7af2231305206ddc6ee9a7e950
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-04 06:47:11 +0200

    Update atto-core to 0.9.4 (#214)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,5 +9,5 @@ object Dependencies {
   lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
   lazy val jawnAst = "org.typelevel" %% "jawn-ast" % "1.1.2"
   lazy val parboiled = "org.parboiled" %% "parboiled" % "2.3.0"
-  lazy val attoCore = "org.tpolecat" %% "atto-core" % "0.9.3"
+  lazy val attoCore = "org.tpolecat" %% "atto-core" % "0.9.4"
 }
