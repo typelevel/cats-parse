8d0dc3f194dd5221ca788af7492a04431159423a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-08-31 13:58:58 +0200

    Update munit, munit-scalacheck to 0.7.29 (#259)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.6.1")
   lazy val cats211 = Def.setting("org.typelevel" %%% "cats-core" % "2.0.0")
-  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.28")
-  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.28")
+  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "0.7.29")
+  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "0.7.29")
   lazy val fastParse = "com.lihaoyi" %% "fastparse" % "2.3.2"
   lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
   lazy val jawnAst = "org.typelevel" %% "jawn-ast" % "1.2.0"
