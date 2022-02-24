4ad47fa3369b5324387f716dffdcc8972e90a5d6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-24 18:06:12 +0100

    Update parboiled to 2.4.0 (#388)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
   lazy val fastParse = "com.lihaoyi" %% "fastparse" % "2.3.3"
   lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
   lazy val jawnAst = "org.typelevel" %% "jawn-ast" % "1.3.2"
-  lazy val parboiled = "org.parboiled" %% "parboiled" % "2.3.0"
+  lazy val parboiled = "org.parboiled" %% "parboiled" % "2.4.0"
   lazy val attoCore = "org.tpolecat" %% "atto-core" % "0.9.5"
 }
