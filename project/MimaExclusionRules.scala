2d8e31cde9125516d1910a92afed7a3a8cf4b778
110416 <ito.yo16uh90616@gmail.com>
2022-03-18 00:54:31 +0900

    update: use java.util.BitSet on js platform (#384)

---
+++ project/MimaExclusionRules.scala
@@ -0,0 +1,29 @@
+import com.typesafe.tools.mima.core.ProblemFilters
+import com.typesafe.tools.mima.core.IncompatibleMethTypeProblem
+import com.typesafe.tools.mima.core.IncompatibleResultTypeProblem
+import com.typesafe.tools.mima.core.DirectMissingMethodProblem
+object MimaExclusionRules {
+  val parserImpl = Seq(
+    "cats.parse.Parser#Impl.mergeCharIn",
+    "cats.parse.Parser#Impl.mergeStrIn",
+    "cats.parse.Parser#Impl#CharIn.copy",
+    "cats.parse.Parser#Impl#CharIn.apply",
+    "cats.parse.Parser#Impl#CharIn.this"
+  ).map(ProblemFilters.exclude[IncompatibleMethTypeProblem](_)) ++ Seq(
+    "cats.parse.Parser#Impl#StringIn.parseMut",
+    "cats.parse.Parser#Impl.stringIn",
+    "cats.parse.Parser#Impl#CharIn.copy$default$2",
+    "cats.parse.Parser#Impl#CharIn.bitSet",
+    "cats.parse.Parser#Impl#CharIn._2"
+  ).map(ProblemFilters.exclude[IncompatibleResultTypeProblem](_)) ++ Seq(
+    "cats.parse.Parser#Impl.mergeCharIn",
+    "cats.parse.Parser#Impl.mergeStrIn"
+  ).map(ProblemFilters.exclude[DirectMissingMethodProblem](_))
+  // TODO: Remove these rules in future release.
+  val bitSetUtil = Seq(
+    "cats.parse.BitSetUtil.isSingleton",
+    "cats.parse.BitSetUtil.isSet"
+  ).map(ProblemFilters.exclude[IncompatibleMethTypeProblem](_)) ++ Seq(
+    "cats.parse.BitSetUtil.bitSetFor"
+  ).map(ProblemFilters.exclude[IncompatibleResultTypeProblem](_))
+}
