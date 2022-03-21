4131ef028e7fe2190796d8536f83f6fd1a94f80d
i10416 <ito.yo16uh90616@gmail.com>
2022-03-22 02:14:06 +0900

    rm comment

--- core/shared/src/test/scala/cats/parse/BitSetTest.scala
+++ core/shared/src/test/scala/cats/parse/BitSetTest.scala
@@ -27,7 +27,6 @@ class BitSetTest extends munit.ScalaCheckSuite {
   // TODO: Remove isScalaJs/isScalaJvm in next minor version update. See https://github.com/typelevel/cats-parse/issues/391.
   test("isScalaJs/isScalaJvm is consistent") {
     if (BitSetUtil.isScalaJs || BitSetUtil.isScalaJvm) {
-      // This will need to be updated if we ever add scala-native
       assert(!(BitSetUtil.isScalaJs && BitSetUtil.isScalaJvm))
       assert(BitSetUtil.isScalaJs || BitSetUtil.isScalaJvm)
       assert(BitSetUtil.isScalaJs ^ BitSetUtil.isScalaJvm)
