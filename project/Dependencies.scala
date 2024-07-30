7f7bda9c3128a6147dbed66a403fa42bc8cd3941
Andrew Valencik <andrew.valencik@shopify.com>
2024-07-30 19:26:01 -0400

    Update munit deps to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.12.0")
   lazy val cats211 = Def.setting("org.typelevel" %%% "cats-core" % "2.0.0")
-  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0-M10")
-  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
+  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0")
+  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0")
   lazy val fastParse = "com.lihaoyi" %% "fastparse" % "3.1.1"
   lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
   lazy val jawnAst = Def.setting("org.typelevel" %% "jawn-ast" % "1.6.0")
