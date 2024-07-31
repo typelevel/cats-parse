60eac5bada33c9756377adcee03ced01928db808
Andrew Valencik <valencik@users.noreply.github.com>
2024-07-30 21:49:46 -0400

    Support Scala native 0.5, Update deps (#602)
    * Update scala-library to 2.13.14
    
    * Update cats to 2.12.0
    
    * Update scala-native to 0.5.4
    
    * Update munit deps to 1.0.0
    
    * Fix munit deps for Scala 2.11
    
    * Fix build.sbt formatting
    
    * Fix repeated naming warning / error
    
    * Reset tlVersionIntroduced for Scala Native 0.5
    
    Trying to follow the approach taken in
    https://github.com/typelevel/cats/pull/4585

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,12 @@ import sbt._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
-  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.11.0")
+  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.12.0")
   lazy val cats211 = Def.setting("org.typelevel" %%% "cats-core" % "2.0.0")
-  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0-M10")
-  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
+  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0")
+  lazy val munit211 = Def.setting("org.scalameta" %%% "munit" % "1.0.0-M10")
+  lazy val munitScalacheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0")
+  lazy val munitScalacheck211 = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
   lazy val fastParse = "com.lihaoyi" %% "fastparse" % "3.1.1"
   lazy val parsley = "org.http4s" %% "parsley" % "1.5.0-M3"
   lazy val jawnAst = Def.setting("org.typelevel" %% "jawn-ast" % "1.6.0")
